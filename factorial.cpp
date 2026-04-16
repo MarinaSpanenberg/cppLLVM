d851f8820995c393bdebf26f91c5a3b5149d8c65
Marina Barbosa Spanenberg <marinabspanenberg@gmail.com>
2026-04-16 18:53:23 -0300

    feat: Add verificação de número positivo no bloco de entrada

--- factorial.cpp
+++ factorial.cpp
@@ -9,38 +9,49 @@ using namespace llvm;
 
 int main() {
 
+    // ambiente global dos objetos llvm
     LLVMContext context;
 
     // criacao do modulo
     Module module ("factorial_module", context);
-    IRBuilder<> ir_builder(context);
+    IRBuilder<> ir_builder(context); // define o builder como sendo o context
 
-    // define o tipo da funcao (int64_t)
-    auto i64 = Type::getInt64Ty(context);
-    auto func_type = FunctionType::get(i64, {i64}, false);
+    // define o tipo da funcao (int64_t) e seus parametros, retorno e quant de argumentos
+    Type *i64 = Type::getInt64Ty(context);
+    FunctionType *func_type = FunctionType::get(i64, {i64}, false);
     
-    // cria a funcao
+    // cria a funcao, deixando ela visivel para outros módulos e a nomeando
     Function *function = Function::Create(func_type, Function::ExternalLinkage, "factorial", &module);
 
     // nomeia o parametro
     Value* n = function->arg_begin();
     n->setName("n");
 
-    // criacao dos blocos
+    // criacao dos blocos (instrucoes da funcao)
     BasicBlock* entry = BasicBlock::Create(context, "entry", function);
     BasicBlock* loop = BasicBlock::Create(context, "loop", function);
     BasicBlock* end = BasicBlock::Create(context, "end", function);
 
-    ir_builder.SetInsertPoint(entry);
-    ir_builder.CreateBr(loop);
+    // logica da funcao
 
+    // inicio meu builder no bloco de entrada
+    ir_builder.SetInsertPoint(entry);
+
+    // verifica se o valor é positivo, ou seja n >= 0
+    Value* is_pos = ir_builder.CreateICmpSGE(n, ConstantInt::get(i64, 0), "is_pos");
+
+    // redireciona a execucao para bloco do laco de repeticao se o nmr for positivo
+    // senao vai direto pro end
+    ir_builder.CreateCondBr(is_pos, loop, end);
+
+    //inicio do bloco do laco
     ir_builder.SetInsertPoint(loop);
 
-    // acumulador res iniciando em 1
+    // phinode para acumulador res iniciando em 1 e ocupando 2 nmrs reservados
     const auto res = ir_builder.CreatePHI(i64, 2);
     res->addIncoming(ConstantInt::get(i64, 1), entry);
 
-    // contador i iniciando em n
+    // phinode para contador i iniciando em n
     const auto i = ir_builder.CreatePHI(i64, 2, "i");
     i->addIncoming(n, entry);
 
@@ -53,13 +64,27 @@ int main() {
     //i = i - 1
     Value* next_i = ir_builder.CreateSub(i, ConstantInt::get(i64, 1), "next_i");
     
+    // atualizacao dos phinodes
     res->addIncoming(next_res, loop);
     i->addIncoming(next_i, loop);
 
+    // if i > 1, continua no loop, senao vai pro end
     ir_builder.CreateCondBr(cmp, loop, end);
 
+    // bloco final
     ir_builder.SetInsertPoint(end);
-    ir_builder.CreateRet(res);
+
+    // phinode para valor final do retorno
+    const auto final_res = ir_builder.CreatePHI(i64, 2);
+
+    // se vem do loop, retorna o fatorial
+    final_res->addIncoming(res, loop);
+
+    // se vem do entry, ou seja, n < 0, retorna 0
+    final_res->addIncoming(ConstantInt::get(i64, 0), entry);
+
+    // retorno do valor final
+    ir_builder.CreateRet(final_res);
 
     // verifica a funcao
     if (verifyFunction(*function, &errs())) {
